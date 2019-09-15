b93af83668e074ac710935d8db720bd3b048935a
Emily <walker.emily010@gmail.com>
2019-09-15 16:46:01 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Emily Walker on 9/15/19.
+//  Copyright © 2019 Emily Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
